a3f28d8de5338964106bdb190dea73e4126671a0
yuenhsi <yuenhsi93@gmail.com>
2017-01-13 15:39:17 -0800

    recreate storyboard

--- Wine Garden/Wine Garden/MainVC.swift
+++ Wine Garden/Wine Garden/MainVC.swift
@@ -43,12 +43,16 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        return WineCell()
     }
 }
 
