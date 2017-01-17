ff4d352ab28c320e9c1d54701f2f14b05b77bb53
yuenhsi <yuenhsi93@gmail.com>
2017-01-17 14:37:14 -0800

    add multi selection, currently breaks while scrolled

--- Wine Garden/Wine Garden/FilterVC.swift
+++ Wine Garden/Wine Garden/FilterVC.swift
@@ -17,19 +17,31 @@ class FilterVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Pick a filter"
         tableView.delegate = self
         tableView.dataSource = self
-
-        // Do any additional setup after loading the view.
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "WineFilterCell")!
-        return cell
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "WineFilterCell") as? WineFilterCell {
+            // the below should be within WineFilterCell class
+            cell.textLabel?.text = Array(options.keys).sorted()[indexPath.row]
+            cell.accessoryType = cell.isSelected ? .checkmark : .none
+            return cell
+        }
+        return UITableViewCell()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return Array(options.keys).count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .none
     }
 
 }
