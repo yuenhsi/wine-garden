edea227e507cce0ed453cf5f2bf7c1bceef80903
yuenhsi <yuenhsi93@gmail.com>
2017-01-18 14:41:06 -0800

    remove bool array for keeping track of selections

--- Wine Garden/Wine Garden/FilterVC.swift
+++ Wine Garden/Wine Garden/FilterVC.swift
@@ -16,28 +16,29 @@ class FilterVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var navTitle: String!
     var selectedIDs: [Int]!
     
-    var keys: [String]!
-    var selections: [Bool]!
+    var keys: [String]! // actual ordering of the table
+//    var selections: [Bool]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "Pick a filter"
+        title = navTitle.capitalized
         keys = Array(options.keys).sorted()
-        selections = [Bool](repeating: false, count: options.count)
+//        selections = [Bool](repeating: false, count: options.count)
         
         tableView.delegate = self
         tableView.dataSource = self
     }
     
     func getSelectedIDs() -> [Int] {
-        var ids = [Int]()
-        for (index, selection) in selections.enumerated() {
-            if selection {
-                ids.append(options[keys[index]]!)
-            }
-        }
-        return ids
+//        var ids = [Int]()
+//        for (index, selection) in selections.enumerated() {
+//            if selection {
+//                ids.append(options[keys[index]]!)
+//            }
+//        }
+//        return ids
+        return selectedIDs
     }
     
     @IBAction func selectBtnPressed(_ sender: Any) {
@@ -45,11 +46,12 @@ class FilterVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     @IBAction func clearBtnPressed(_ sender: Any) {
-        for (index, selection) in selections.enumerated() {
-            if selection {
-                selections[index] = false
-            }
-        }
+//        for (index, selection) in selections.enumerated() {
+//            if selection {
+//                selections[index] = false
+//            }
+//        }
+        selectedIDs.removeAll()
         tableView.reloadData()
     }
     
@@ -59,13 +61,18 @@ class FilterVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "WineFilterCell") as? WineFilterCell {
-            // the below should be within WineFilterCell class
-            cell.textLabel?.text = keys[indexPath.row]
-            if selections[indexPath.row] {
+            let filterName = keys[indexPath.row]
+            cell.textLabel?.text = filterName
+            if selectedIDs.contains(options[filterName]!) {
                 cell.accessoryType = .checkmark
             } else {
                 cell.accessoryType = .none
             }
+//            if selections[indexPath.row] {
+//                cell.accessoryType = .checkmark
+//            } else {
+//                cell.accessoryType = .none
+//            }
             return cell
         }
         return UITableViewCell()
@@ -76,12 +83,20 @@ class FilterVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selections[indexPath.row] = true
+//        selections[indexPath.row] = true
+        let id = options[keys[indexPath.row]]!
+        if !selectedIDs.contains(id) {
+            selectedIDs.append(id)
+        }
         tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
     }
     
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-        selections[indexPath.row] = false
+//        selections[indexPath.row] = false
+        let id = options[keys[indexPath.row]]!
+        if let index = selectedIDs.index(of: id) {
+            selectedIDs.remove(at: index)
+        }
         tableView.cellForRow(at: indexPath)?.accessoryType = .none
     }
     
